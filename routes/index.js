0c8952cb7699fdd87372a37299fc1ab9a57dd565
Daaman23 <46690830+Daaman23@users.noreply.github.com>
2019-06-17 19:05:11 -0400

    Add files via upload
    Added bootstrap to navbar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Aboutme', (req, res, next) => {
+  res.render('Aboutme', {});
+});
+
+router.get('/Projects',(req, res, next) => {
+  res.render('Projects', {});
+});
+
+router.get('/Services',(req, res, next) => {
+  res.render('Services', {});
+});
+
+router.get('/Contactme',(req, res, next) => {
+  res.render('Contactme', {});
+});
+
 module.exports = router;
